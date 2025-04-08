b669103173525d9fca2069d9cd793353ff7ba949
Sandeep Kumar <sandepten@gmail.com>
2025-04-08 14:00:51 +0530

    remove element

---
+++ arrays/RemoveElement.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int totalNonVal = 0, placeIndex = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == val) {
+                continue;
+            }
+            int temp = nums[i];
+            nums[i] = nums[placeIndex];
+            nums[placeIndex] = temp;
+            totalNonVal++;
+            placeIndex++;
+        }
+
+        return totalNonVal;
+    }
+}
